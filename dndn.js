519a628af80fc4cbb59adb72ec4c923d136f0951
amirbots2 <48495961+amirbots2@users.noreply.github.com>
2019-08-20 11:23:02 +0100

    Add files via upload

---
+++ dndn.js
@@ -0,0 +1,57 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+
+console.log("Welcome Again !");
+ 
+ client.on("guildMemberRemove", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+:kissing::heart::eyes: حيااكْ الله  فى سيرفرنا 
+╔╦╦╦═╦╗╔═╦═╦══╦═╗
+║║║║╩╣╚╣═╣║║║║║╩╣
+╚══╩═╩═╩═╩═╩╩╩╩═╝ 
+
+‏نقدم لك دعوة لسموك الراقيَ .
+‏لنحظى بجمال حضورك وتواجدك
+•بتنـورونـا الجميع :wink:
+•اجيتك عشان اقولك نورت سيرفرنا يا غالي راح يكون فيه قيفويات كثيرة و اشياء مرة تعجبك
+•عندك فكره موضوع تفضل شآركنا فيه:jack_o_lantern::gift:
+•واخيرا حييييياكم واكيد وجودكم هو اللي بيدعمنا وراح نكون الافضل.
+شرفنا يا عسل:wink: :kissing_heart:
+
+رابط السيرفر
+https://discord.gg/5x69GGf
+! ᵛᶤʳᵘˢ ™.                              
+الدعووة خاصة لك ي قلبي ... [ ${member}  ]
+**`)
+}).catch(console.error)
+
+})
+ 
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+:kissing::heart:حيااكْ الله  فى سيرفرنا 
+╔╦╦╦═╦╗╔═╦═╦══╦═╗
+║║║║╩╣╚╣═╣║║║║║╩╣
+╚══╩═╩═╩═╩═╩╩╩╩═╝ 
+
+‏نقدم لك دعوة لسموك الراقيَ .
+‏لنحظى بجمال حضورك وتواجدك
+•بتنـورونـا الجميع :wink:
+•اجيتك عشان اقولك نورت سيرفرنا يا غالي راح يكون فيه قيفويات كثيرة و اشياء مرة تعجبك
+•عندك فكره موضوع تفضل شآركنا فيه:jack_o_lantern::gift:
+•واخيرا حييييياكم واكيد وجودكم هو اللي بيدعمنا وراح نكون الافضل.
+شرفنا يا عسل:wink: :kissing_heart:
+
+
+رابط السيرفر
+https://discord.gg/5x69GGf
+! ᵛᶤʳᵘˢ ™.                               
+الدعووة خاصة لك ي قلبي ... [ ${member}  ]
+**`) 
+}).catch(console.error)
+})
+client.login('NjEzMzA4MzA5ODUxNzM0MDQ3.XVvDYQ.83zZxh8gi3LJZmGuAqr_N1OyaNc');
